a11f1d59ae358c51bcb55d2733c9cd33f6400fe2
LavrLitvinov <lux-t@mail.ru>
2021-04-04 11:06:47 +0300

    sum

--- src/main/java/tudelft/sum/TwoNumberSum1.java
+++ src/main/java/tudelft/sum/TwoNumberSum1.java
@@ -1,12 +1,30 @@
 package tudelft.sum;
-
+// самопальный тест
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
-// Source: https://leetcode.com/problems/add-two-numbers/description/
-class TwoNumbersSum {
+public class TwoNumberSum1 {
 
-    public ArrayList<Integer> addTwoNumbers(ArrayList<Integer> first, ArrayList<Integer> second) {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(9,9));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1));
+      //  Collections.addAll(first, 1, 8, 6);
+     //   Collections.addAll(second, 1, 8, 8);
+        ArrayList<Integer> result2 = addTwoNumbers1(first, second);
+        for(int x: result2){
+            System.out.print(x);
+
+        }
+        System.out.println();
+        System.out.println( " " + result2.toString());
+
+    }
+
+
+
+    private static ArrayList<Integer> addTwoNumbers1(ArrayList<Integer> first, ArrayList<Integer> second) {
         Collections.reverse(first);
         Collections.reverse(second);
         int cnt = 0;
@@ -18,17 +36,19 @@ class TwoNumbersSum {
             int firstVal = i < first.size() ? first.get(i) : 0;
             int secondVal = i < second.size() ? second.get(i) : 0;
             int total = firstVal + secondVal + complement;
-            complement = 0;
+           // complement = 0;
             if (total >= 10){
                 complement = 1;
                 total -= 10;
 
             }
             result.add(i, total);
+
         }
         if(complement != 0) result.add(cnt, complement);
 
         Collections.reverse(result);
         return result;
     }
+
 }
--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,56 @@
 package tudelft.sum;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TwoNumbersSumTest {
 
+
+    private  TwoNumbersSum suma;
+    @BeforeEach
+    public void inite(){
+        this.suma = new TwoNumbersSum();
+    }
+
+    @Test
+    public void twoNumber1(){
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(9,0));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1,1));
+        ArrayList<Integer> expectedRezult = new ArrayList<>(Arrays.asList(1,0,1));
+        ArrayList<Integer> rezult = suma.addTwoNumbers(first, second);
+        Assertions.assertEquals(expectedRezult, rezult);
+
+    }
+//
+    @Test
+    public void twoNumber(){
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(5,6));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1,1));
+        ArrayList<Integer> expectedRezult = new ArrayList<>(Arrays.asList(6,7));
+        ArrayList<Integer> rezult = suma.addTwoNumbers(first, second);
+        Assertions.assertEquals(expectedRezult, rezult);
+
+    }
+    @Test
+    public void oneNumber(){
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(9));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1));
+        ArrayList<Integer> expectedRezult = new ArrayList<>(Arrays.asList(1,0));
+        ArrayList<Integer> rezult = suma.addTwoNumbers(first, second);
+        Assertions.assertEquals(expectedRezult, rezult);
+    }
+
+    @Test
+    public void threeNumber1() {
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(9, 0, 5));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(1, 1, 1));
+        ArrayList<Integer> expectedRezult = new ArrayList<>(Arrays.asList(1,0, 1, 6));
+        ArrayList<Integer> rezult = suma.addTwoNumbers(first, second);
+        Assertions.assertEquals(expectedRezult, rezult);
+    }
 }
